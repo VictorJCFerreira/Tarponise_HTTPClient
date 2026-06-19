b228439e1e5e4f215541edbba8bb66c778e7390e
VictorJCFerreira <victorjcferreira462@gmail.com>
2026-06-19 10:15:00 -0300

    feat(build_request): Função para monstar a String do metodo GET com os parametros do Parse feito anteriormente

--- client.py
+++ client.py
@@ -14,4 +14,26 @@ def parse_url(url):
     if parsed.query:
         path += "?" + parsed.query
     
+    # Separa o Host, pois muitos domínios copartilham o mesmo endereço IP físico, ele indica qual aplicação específica 
+    # deve processar a requisição.
+    # A porta é o número da porta TCP onde o servidor está escutando.
+    # Se a URL não especificar uma porta, usamos a porta padrão 80 (HTTP).
+    # E o path é a parte da URL que indica o recurso específico que queremos acessar no servidor. 
+    # Se a URL não tiver um path, usamos "/" como padrão, que geralmente representa a página inicial ou raiz do site.
     return host, port, path
+
+def build_request(host, path, method="GET"):
+    request_line = f"{method} {path} HTTP/1.1\r\n"
+    headers = (
+        f"Host: {host}\r\n"
+        f"User-Agent: meu-client/0.1\r\n"
+        f"Connection: close\r\n"
+    )
+
+    # O "\r\n" extra é a linha em branco que marca o fim dos headers.
+    # Se nao for tratado da forma correta, pode gerar vulnerabilidades como CRLF injection.
+    requisicao = request_line + headers + "\r\n"
+    return requisicao.encode("utf-8")
+
+
+
